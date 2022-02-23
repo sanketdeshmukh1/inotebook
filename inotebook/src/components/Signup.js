e06a429bf4d2b9180f9f5115f443cbb27caca6b3
Sanket Deshmukh <deshmukhsanket19@gmail.com>
2022-02-23 21:57:16 +0530

    signup page is in progress

--- inotebook/src/components/Signup.js
+++ inotebook/src/components/Signup.js
@@ -2,8 +2,46 @@ import React from 'react'
 
 function Signup() {
   return (
-    <div>This is signup page</div>
-  )
+    <>
+    <form >
+      <div className="form-group">
+        <label htmlFor="email">Email address</label>
+        <input
+          type="email"
+          className="form-control my-1"
+          id="email"
+          name="email"
+          aria-describedby="emailHelp"
+          placeholder="Enter email"
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          className="form-control"
+          id="password"
+          name="password"
+          placeholder="Password"
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="password">Confirm Password</label>
+        <input
+          type="password"
+          className="form-control"
+          id="cpassword"
+          name="cpassword"
+          placeholder="Confirm Password"
+        />
+      </div>
+
+      <button type="submit" className="btn btn-primary my-2">
+        Submit
+      </button>
+    </form>
+  </>
+    )
 }
 
 export default Signup
